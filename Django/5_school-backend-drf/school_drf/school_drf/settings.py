ac3d1100aa59149ff0bcf30e1417776281b4813d
Szymon-Budziak <szymonbudziak@gmail.com>
2022-09-29 20:50:05 +0200

    deploying api to the server

--- Django/5_school-backend-drf/school_drf/school_drf/settings.py
+++ Django/5_school-backend-drf/school_drf/school_drf/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'school-api-react.herokuapp.com']
 
 # Application definition
 
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
