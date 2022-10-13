42549d1e11cbffcf579462bb818d75897b0b9342
Szymon-Budziak <szymonbudziak@gmail.com>
2022-10-13 22:29:04 +0200

    Update of setup.py file

--- Django/5_school-backend-drf/school_drf/setup.py
+++ Django/5_school-backend-drf/school_drf/setup.py
@@ -1,6 +1,11 @@
 import os
 import django
 from django.contrib.auth.models import User
+from django.core.management.utils import get_random_secret_key
+
+# generate random SECRET_KEY and add it to the .env file
+with open(".env", "w") as f:
+    f.write(f"SECRET_KEY = '{get_random_secret_key()}'")
 
 # migrate
 os.environ['DJANGO_SETTINGS_MODULE'] = 'rest_api.settings'
