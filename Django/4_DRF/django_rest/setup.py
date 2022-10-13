16be1bd1ea1b798842bb5fa21f687a212eb4612a
Szymon-Budziak <szymonbudziak@gmail.com>
2022-10-13 21:18:44 +0200

    Repaired setup.py file

--- Django/4_DRF/django_rest/setup.py
+++ Django/4_DRF/django_rest/setup.py
@@ -9,10 +9,10 @@ django.core.management.execute_from_command_line(['manage.py', 'migrate'])
 from bookreview.models import Author, Book
 
 # create two Author objects and save them
-author1 = Author("Adam", "Mickiewicz")
+author1 = Author(first_name="Adam", last_name="Mickiewicz")
 author1.save()
 
-author2 = Author("Jane", "Austen")
+author2 = Author(first_name="Jane", last_name="Austen")
 author2.save()
 
 # add books to authors
