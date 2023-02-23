54b370ba5f0d53c1efe779835268b7e7755d69b6
Szymon-Budziak <szymonbudziak@gmail.com>
2023-02-23 20:05:38 +0100

    Changed order of files

--- Django/2_Django_user_management/awesome_website/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path("dashboard/", views.dashboard, name="dashboard"),
-    path("accounts/", include("django.contrib.auth.urls")),
-    path("register/", views.register, name="register"),
-    path("oauth/", include("social_django.urls")),
-]
