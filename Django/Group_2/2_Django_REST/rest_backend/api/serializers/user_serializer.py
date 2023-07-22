c0a2e02f7eaf754192a20890f144a328907669f5
Szymon-Budziak <szymonbudziak@gmail.com>
2023-07-22 16:12:00 +0200

    Created user view and serializer

---
+++ Django/Group_2/2_Django_REST/rest_backend/api/serializers/user_serializer.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    # password = # create minimum length
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'email')
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'style': {'input_type': 'password'},
+            }
+        }
+
+    def create(self, validated_data):
+        user = User(username=validated_data['username'], email=validated_data['email'])
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
