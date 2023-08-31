8665f41807d1e2d4b10d5d2157586e4a0f398dcc
Szymon-Budziak <szymonbudziak@gmail.com>
2023-08-31 21:10:52 +0200

    Initial code of Django REST project

---
+++ Django/Group_3/3_Django_REST/api/models/book.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+from . import Author
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=128)
+    author_id = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
